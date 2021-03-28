306345bb929ced8d8244f163ce4d3732a6c0a0a2
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-28 13:56:54 +0300

    Allow pluginrc from any plugin

--- xxh-config.fish
+++ xxh-config.fish
@@ -3,7 +3,7 @@
 set -U fish_greeting ""
 set CURRENT_DIR (dirname (realpath (status current-filename)))
 
-set -l dirs $CURRENT_DIR/../../../plugins/*-fish-*/build
+set -l dirs $CURRENT_DIR/../../../plugins/*/build
 for pluginrc_file in (find $dirs -type f -name '*pluginrc.fish' -printf '%f\t%p\n' 2>/dev/null | sort -k1 | cut -f2)
   if  test  -f $pluginrc_file 
     set plugin_name (basename (dirname (dirname $pluginrc_file)))
