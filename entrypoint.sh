f5dc7ba68a4216e437e954a213a9ecf4464db7d8
Andy Kipp <anki-code@users.noreply.github.com>
2021-05-27 18:38:31 +0300

    Added XXH_SHELL

--- entrypoint.sh
+++ entrypoint.sh
@@ -121,6 +121,7 @@ else
   export XDGPATH=$XXH_HOME
 fi
 
+export XXH_SHELL=fish
 export XDG_CONFIG_HOME=$XDGPATH/.config
 export XDG_DATA_HOME=$XDGPATH/.local/share
 export XDG_CACHE_HOME=$XDGPATH/.cache
